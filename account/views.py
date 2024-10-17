f0b5ca8195ff947155caa8f1429539baff8f1a1b
student06 <jakub.gola2@teb.edu.pl>
2024-10-17 13:20:07 +0200

    Commit 17.10.2024

--- account/views.py
+++ account/views.py
@@ -1,11 +1,25 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from account.forms import LoginForm
 
 
 def user_login(request):
     if request.method == 'POST':
-        pass
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            user = authenticate(username=cleaned_data['username'],
+                                password=cleaned_data['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Uwierzytelnienie zakończyło się suksesem')
+                else:
+                    return HttpResponse('Konto jest zablokowane')
+            else:
+                return HttpResponse('Nieprawidłowe dane logowania')
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
