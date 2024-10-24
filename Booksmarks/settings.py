c1c4b3905efb41e00904e355ebf446fc077b8182
student06 <jakub.gola2@teb.edu.pl>
2024-10-24 14:08:55 +0200

    Commit 24.10.2024

--- Booksmarks/settings.py
+++ Booksmarks/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import LOGOUT_REDIRECT_URL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'dashboard'
--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,5 @@
+from tkinter.font import names
+
 from django.urls import path
 
 from account import views
@@ -5,5 +7,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     #path('login/', views.user_login,name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('', views.dashboard, name='dashboard'),
 ]
